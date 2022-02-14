5fe3e88e8b9ab9cd8614e15ffa3f338b6d722903
Brandon G <goewertb@yahoo.com>
2022-02-14 14:33:21 -0500

    Add project files.

---
+++ SkateShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SkateShop.MvcApplication" Language="C#" %>
---
+++ SkateShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkateShop.Startup))]
+namespace SkateShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
